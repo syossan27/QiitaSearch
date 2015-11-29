aa4daf7c72819aeef94e63563b353e74ae08ec78
syossan27 <wisdom1027@gmail.com>
2015-11-30 01:34:05 +0900

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,7 +1,8 @@
 var submit = document.getElementById("submit");
 submit.addEventListener('click',function(){
 	var search_word = document.getElementById("search").value;
-	var url = "https://innova.qiita.com/search?utf8=✓&sort=rel&q=-tag:日報+"+ search_word;
+	var domain = "https://innova.qiita.com"
+	var url = domain+"/search?utf8=✓&sort=rel&q=-tag:日報+"+ search_word;
 	window.open(url, '_blank');
 	return false;
 },false);
